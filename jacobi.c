885ef9020c3820d53778d5189f397e01ee6d8d77
textfox <ec15201@my.bristol.ac.uk>
2017-10-12 20:28:38 +0100

    reverted

--- jacobi.c
+++ jacobi.c
@@ -27,7 +27,7 @@
 static int N;
 static int MAX_ITERATIONS;
 static int SEED;
-static double CONVERGENCE_THRESHOLD;
+static float CONVERGENCE_THRESHOLD;
 
 #define SEPARATOR "------------------------------------\n"
 
@@ -39,30 +39,30 @@ void parse_arguments(int argc, char *argv[]);
 
 // Run the Jacobi solver
 // Returns the number of iterations performed
-int run(double *A, double *b, double *x, double *xtmp)
+int run(float *A, float *D, float *b, float *x, float *xtmp)
 {
   int itr;
   int row, col;
-  double dot;
-  double diff;
-  double sqdiff;
-  double *ptrtmp;
+  float dot;
+  float diff;
+  float sqdiff;
+  float *ptrtmp;
 
   // Loop until converged or maximum iterations reached
   itr = 0;
   do
   {
-    // Perfom Jacobi iteration
-    for (row = 0; row < N; row++)
-    {
-      dot = 0.0;
-      for (col = 0; col < N; col++)
-      {
-        if (row != col)
-          dot += A[row + col*N] * x[col];
-      }
-      xtmp[row] = (b[row] - dot) / A[row + row*N];
+
+// Perfom Jacobi iteration (can be extracted into function)
+for (row = 0; row < N; row++)
+{
+  dot = 0.0;
+  for (col = 0; col < N; col++)
+  {
+      dot += A[col + row*N] * x[col];
     }
+    xtmp[row] = (b[row] - dot) * D[row];
+}
 
     // Swap pointers
     ptrtmp = x;
@@ -87,10 +87,13 @@ int main(int argc, char *argv[])
 {
   parse_arguments(argc, argv);
 
-  double *A    = malloc(N*N*sizeof(double));
-  double *b    = malloc(N*sizeof(double));
-  double *x    = malloc(N*sizeof(double));
-  double *xtmp = malloc(N*sizeof(double));
+  float *Aprime = malloc(N*N*sizeof(float));
+  float *A    = malloc(N*N*sizeof(float));
+  float *D    = malloc(N*sizeof(float));
+  float *b    = malloc(N*sizeof(float));
+  float *x    = malloc(N*sizeof(float));
+  float *xtmp = malloc(N*sizeof(float));
+
 
   printf(SEPARATOR);
   printf("Matrix size:            %dx%d\n", N, N);
@@ -104,31 +107,42 @@ int main(int argc, char *argv[])
   srand(SEED);
   for (int row = 0; row < N; row++)
   {
-    double rowsum = 0.0;
+    float rowsum = 0.0;
     for (int col = 0; col < N; col++)
     {
-      double value = rand()/(double)RAND_MAX;
-      A[row + col*N] = value;
+      float value = rand()/(float)RAND_MAX;
+      A[col + row*N] = value;
+      Aprime[col + row*N] = value;
       rowsum += value;
     }
     A[row + row*N] += rowsum;
-    b[row] = rand()/(double)RAND_MAX;
+    Aprime[row + row*N] += rowsum;
+    b[row] = rand()/(float)RAND_MAX;
     x[row] = 0.0;
+    D[row] = 0.0;
+  }
+
+  //Extract diagonal (can be extracted into function)
+
+  for (int row = 0; row < N; row++)
+  {
+      D[row] = 1 / A[row + row*N];
+      A[row + row*N] = 0.0;
   }
 
   // Run Jacobi solver
   double solve_start = get_timestamp();
-  int itr = run(A, b, x, xtmp);
+  int itr = run(A, D, b, x, xtmp);
   double solve_end = get_timestamp();
 
   // Check error of final solution
-  double err = 0.0;
+  float err = 0.0;
   for (int row = 0; row < N; row++)
   {
-    double tmp = 0.0;
+    float tmp = 0.0;
     for (int col = 0; col < N; col++)
     {
-      tmp += A[row + col*N] * x[col];
+      tmp += Aprime[col + row*N] * x[col];
     }
     tmp = b[row] - tmp;
     err += tmp*tmp;

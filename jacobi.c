86f8060d555a93352655fd998b83b6e3f70006b9
textfox <ec15201@my.bristol.ac.uk>
2017-11-09 13:23:24 +0000

    add D

--- jacobi.c
+++ jacobi.c
@@ -55,7 +55,7 @@ int run(float *A, float *D, float *b, float *x, float *xtmp)
   {
 
 // Perfom Jacobi iteration (can be extracted into function)
-#pragma omp parallel for shared(A, x, b, xtmp) private(dot)
+#pragma omp parallel for shared(A, x, b, D, xtmp) private(dot)
     for (row = 0; row < N; row++)
     {
       dot = 0.0;

ee609995b18273c1da779b67da05445b203e8095
textfox <ec15201@my.bristol.ac.uk>
2017-12-07 12:43:46 +0000

    affinity

--- jacobi.c
+++ jacobi.c
@@ -133,26 +133,6 @@ int main(int argc, char *argv[])
     b[row] = rand()/(float)RAND_MAX;
   }
 
-  /*srand(SEED);
-  int row, col;
-  #pragma omp parallel for shared(A, Aprime, b, x, D)
-  for (row = 0; row < N; row++)
-  {
-    float rowsum = 0.0;
-    for (col = 0; col < N; col++)
-    {
-      float value = rand_r(&SEED)/(float)RAND_MAX;
-      A[col + row*N] = value;
-      Aprime[col + row*N] = value;
-      rowsum += value;
-    }
-    A[row + row*N] += rowsum;
-    Aprime[row + row*N] += rowsum;
-    b[row] = rand()/(float)RAND_MAX;
-    x[row] = 0.0;
-    D[row] = 0.0;
-  }*/
-
   //Extract diagonal (can be extracted into function)
 
   for (row = 0; row < N; row++)
